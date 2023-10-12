f800ae82342d0572664a4c57a8ce35394753e5e8
DroidekaMuSo <diego.munoz_solis@hotmail.com>
2023-10-11 19:29:24 -0600

    Class 10

---
+++ classes/class10/class.py
@@ -0,0 +1,20 @@
+# def divide(a: int, b: int):
+#     if b == 0:
+#         return "Cannot divide using 0"
+#
+#     return a/b
+#
+#
+# print(divide(9, 0))
+
+# "A2XhcPJPD9BuMKELKz12a1oMNNCZx"
+
+try:
+    num1 = int(input("Insert your first number"))
+    num2 = int(input("Insert your second number"))
+
+    print(num1 / num2)
+except TypeError as err:
+    print(err)
+    print("Incorrect data")
+
